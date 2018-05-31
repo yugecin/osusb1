379a0acb6eb041dd2f5b97861995b1b389b9c196
yugecin <yugecin@users.noreply.github.com>
2018-05-31 22:12:42 +0200

    dottet torus

--- ztor.cs
+++ ztor.cs
@@ -5,11 +5,11 @@ namespace osusb1 {
 partial class all {
 	class Ztor : Z {
 
-		Pixelscreen screen = new Pixelscreen(640, 480, 4);
+		Pixelscreen screen = new Pixelscreen(640, 480, 1);
 
 		vec3 mid = v3(0f, 50f, 90f);
 
-		const int DIVH = 30;
+		const int DIVH = 35;
 		const int DIVV = 15;
 		const int RH = 40;
 		const int RV = 10;
@@ -52,22 +52,44 @@ partial class all {
 
 		public override void draw(SCENE scene) {
 			turn(this._points, this.points, mid, scene.reltime / 5f + mousex, scene.reltime / 10f + mousey);
+			/*
 			if (scene.g != null) {
 				foreach (vec3 v in this._points) {
 					vec4 r = p.Project(v);
 					scene.g.FillRectangle(new SolidBrush(Color.Green), r.x - 1, r.y - 1, 2, 2);
 				}
 			}
+			*/
 			screen.clear();
 			foreach (Rect r in rects) {
 				if (!r.shouldcull()) {
-					vec3 col = v3(.5f, .68f, .98f);
-					col *= .1f + .9f * (r.surfacenorm().norm() ^ r.rayvec().norm());
-					r.color = r.tri1.color = r.tri2.color = col.col();
+					//vec3 col = v3(.5f, .68f, .98f);
+					//col *= .1f + .9f * (r.surfacenorm().norm() ^ r.rayvec().norm());
+					//r.color = r.tri1.color = r.tri2.color = col.col();
 					r.draw(screen);
 				}
 			}
-			screen.draw(scene);
+			//screen.draw(scene);
+			if (scene.g != null) foreach (Rect r in rects) {
+				if (!r.shouldcull()) {
+					vec4 loc = p.Project(lerp(r.pts[r.a], r.pts[r.d], .5f));
+					int lx = (int) loc.x;
+					int ly = (int) loc.y;
+					if (lx < 0 || 640 <= lx || ly < 0 || 480 <= ly) {
+						continue;
+					}
+					object o = screen.owner[lx, ly];
+					if (!(o is Tri)) {
+						continue;
+					}
+					if (((Tri) o).owner != r) {
+						continue;
+					}
+					vec3 col = v3(.5f, .68f, .98f);
+					col *= .1f + .9f * (r.surfacenorm().norm() ^ r.rayvec().norm());
+					scene.g.FillRectangle(new SolidBrush(col.col()), lx - 1, ly - 1, 3, 3);
+				}
+			}
 		}
 
 		public override void fin(Writer w) {
