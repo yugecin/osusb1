f7055c994d73b5e7a380a777bf8aa1da99354ee3
yugecin <yugecin@users.noreply.github.com>
2018-07-31 23:41:45 +0200

    font rendering using cubes

--- form.cs
+++ form.cs
@@ -127,7 +127,8 @@ partial class all {
 		//zs.Add(new Zrub(00000, 40000));
 		//zs.Add(new Ztestcube3(00000, 20000));
 		//zs.Add(new Ztestcube4(00000, 20000));
-		zs.Add(new Ztestfont(00000, 20000));
+		//zs.Add(new Ztestfont(00000, 20000));
+		zs.Add(new Ztestfont2(00000, 20000));
 		zs.Add(new Z0010spect(50000, 60000));
 		zs.Add(new Ztestcube2(60000, 70000));
 		//zs.Add(new Ztor(70000, 80000));
---
+++ ztestfont2.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Drawing;
+using System.Text;
+
+namespace osusb1 {
+partial class all {
+	class Ztestfont2 : Z {
+
+		vec3 mid = v3(0f, -30f, 100f);
+
+		vec3[] points;
+		vec3[] _points;
+		Rect[] rects;
+		Orect[] orects;
+		int pointcount;
+
+		const int
+			PA = 1,
+			PB = 0,
+			PC = 4,
+			PD = 5,
+			PE = 3,
+			PF = 2,
+			PG = 6,
+			PH = 7;
+
+		public Ztestfont2(int start, int stop) {
+			this.start = start;
+			this.stop = stop;
+
+			string text = "Hello!";
+
+			int width = font.textWidth(text);
+			int fullsize = (width - (text.Length - 1)) * font.charheight;
+			pointcount = 0;
+			points = new vec3[fullsize * 8];
+			rects = new Rect[fullsize * 6];
+
+			const int SIZE = 2;
+
+			Color[] cols = { Color.Cyan, Color.Lime, Color.Red, Color.Blue, Color.Yellow, Color.Orange };
+			vec3 topleft = mid - v3(width / 2f * SIZE, 0f, -font.charheight / 2f * SIZE);
+			for (int i = 0; i < text.Length; i++) {
+				int c = text[i] - 32;
+				vec3 pos = v3(topleft);
+				for (int j = 0; j < font.charheight; j++) {
+					int cw = font.charwidth[c];
+					for (int k = 0; k < font.charwidth[c]; k++) {
+						if (((font.chardata[c][j] >> (cw - k - 1)) & 1) == 1) {
+							int pidx = pointcount * 8;
+							vec3 basepoint = pos + v3(k * SIZE, 0f, 0f);
+							new Pcube(points, pidx).set(basepoint, SIZE, SIZE, SIZE);
+							Cube cube = new Cube(cols, _points, pidx);
+							for (int z = 0; z < 6; z++) {
+								rects[pointcount * 6 + z] = cube.rects[z];
+							}
+							pointcount++;
+						}
+					}
+					pos.z -= SIZE;
+				}
+				topleft.x += (font.charwidth[c] + 1) * SIZE;
+			}
+
+			_points = new vec3[pointcount * 8];
+			orects = new Orect[pointcount * 6];
+			for (int i = 0; i < pointcount * 6; i++) {
+				rects[i].pts = _points;
+				rects[i].tri1.points = _points;
+				rects[i].tri2.points = _points;
+				orects[i] = new Orect(rects[i], 0);
+			}
+		}
+
+		public override void draw(SCENE scene) {
+			turn(_points, points, mid, 800f * scene.progress + mousex, 1200f * scene.progress + mousey);
+
+			foreach (Orect r in orects) {
+				r.update(scene);
+			}
+		}
+
+		public override void fin(Writer w) {
+			foreach (Orect r in orects) {
+				r.fin(w);
+			}
+		}
+
+	}
+}
+}
