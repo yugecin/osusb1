9b9f67dca43585b34f389bf823ff519b4f884c18
yugecin <yugecin@users.noreply.github.com>
2018-08-03 22:47:19 +0200

    add smooth fft

--- fft.cs
+++ fft.cs
@@ -11,6 +11,7 @@ class FFT {
 
 	List<FRAME> frames;
 	public FRAME frame;
+	public FRAME smoothframe;
 
 	public class FRAME {
 		public int time;
@@ -47,12 +48,29 @@ class FFT {
 	}
 
 	public void Update(int time) {
-		this.frame = frames[0];
-		foreach (FRAME f in frames) {
-			this.frame = f;
-			if (f.time > time) {
+		int idx = 0;
+		for (; idx < frames.Count; idx++) {
+			frame = frames[idx];
+			if (frames[idx].time > time) {
 				break;
 			}
+		}
+		const float FALLOFF = .002f;
+		const float FALLUP = .009f;
+		smoothframe = new FRAME();
+		smoothframe.time = time;
+		smoothframe.values = new float[FREQS];
+		for (int j = 0; j < FREQS; j++) {
+			float value = 0f;
+			for (int i = max(0, idx - 15); i <= idx; i++) {
+				FRAME pf = frames[i];
+				value = max(value, pf.values[j] - (frame.time - pf.time) * FALLOFF);
+			}
+			for (int i = idx + 1; i < min(frames.Count - 1, idx + 15); i++) {
+				FRAME pf = frames[i];
+				value = max(value, pf.values[j] - (pf.time - frame.time) * FALLUP);
+			}
+			smoothframe.values[j] = value;
 		}
 	}
 	
