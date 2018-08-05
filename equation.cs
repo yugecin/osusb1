7aaec9f52278665c22b2d2411a375cba73ad9806
yugecin <yugecin@users.noreply.github.com>
2018-08-05 18:21:02 +0200

    add some light in the tunnel

--- equation.cs
+++ equation.cs
@@ -4,98 +4,98 @@ using System.Text;
 
 namespace osusb1 {
 partial class all {
+	public static float eq_linear(float t) {
+		return t;
+	}
+	public static float eq_in_quad(float t) {
+		return t * t;
+	}
+	public static float eq_out_quad(float t) {
+		return -1f * t * (t - 2f);
+	}
+	public static float eq_in_out_quad(float t) {
+		t *= 2f;
+		if (t < 1f) return .5f * t * t;
+		t -= 1f;
+		return -.5f * (t * (t - 2f) - 1f);
+	}
+	public static float eq_in_cubic(float t) {
+		return t * t * t;
+	}
+	public static float eq_out_cubic(float t) {
+		t -= 1f;
+		return t * t * t + 1f;
+	}
+	public static float eq_in_out_cubic(float t) {
+		t *= 2f;
+		if (t < 1f) return .5f * t * t * t;
+		t -= 2f;
+		return .5f * (t * t * t + 2f);
+	}
+	public static float eq_in_quart(float t) {
+		return t * t * t * t;
+	}
+	public static float eq_out_quart(float t) {
+		t -= 1f;
+		return -1f * (t * t * t * t - 1f);
+	}
+	public static float eq_in_out_quart(float t) {
+		t *= 2f;
+		if (t < 1f) return .5f * t * t * t * t;
+		t -= 2f;
+		return -.5f * (t * t * t * t - 2f);
+	}
+	public static float eq_in_quint(float t) {
+		return t * t * t * t * t;
+	}
+	public static float eq_out_quint(float t) {
+		t -= 1f;
+		return t * t * t * t * t + 1f;
+	}
+	public static float eq_in_out_quint(float t) {
+		t *= 2f;
+		if (t < 1f) return .5f * t * t * t * t * t;
+		t -= 2f;
+		return .5f * (t * t * t * t * t + 2f);
+	}
+	public static float eq_in_sine(float t) {
+		return -1f * cos(t * PI2) + 1f;
+	}
+	public static float eq_out_sine(float t) {
+		return sin(t * PI2);
+	}
+	public static float eq_in_out_sine(float t) {
+		return (cos(PI * t) - 1f) / -2f;
+	}
+	public static float eq_in_expo(float t) {
+		return t == 0 ? 0 : pow(2, 10f * (t - 1));
+	}
+	public static float eq_out_expo(float t) {
+		return t == 1 ? 1 : -pow(2, -10f * t) + 1;
+	}
+	public static float eq_in_out_expo(float t) {
+		if (t == 0f || t == 1f) return t;
+		t *= 2f;
+		if (t < 1f) return .5f * pow(2, 10f * (t - 1));
+		t -= 1f;
+		return .5f * (-pow(2, -10f * t) + 2f);
+	}
+	public static float eq_in_circ(float t) {
+		return -1f * (sqrt(1f - t * t) - 1f);
+	}
+	public static float eq_out_circ(float t) {
+		t -= 1f;
+		return sqrt(1f - t * t);
+	}
+	public static float eq_in_out_circ(float t) {
+		t *= 2f;
+		if (t < 1f) return -.5f * (sqrt(1f - t * t) - 1f);
+		t -= 2f;
+		return .5f * (sqrt(1f - t * t) + 1f);
+	}
+
 	class Equation {
 		public delegate float Eq(float t);
-
-		public static float eq_linear(float t) {
-			return t;
-		}
-		public static float eq_in_quad(float t) {
-			return t * t;
-		}
-		public static float eq_out_quad(float t) {
-			return -1f * t * (t - 2f);
-		}
-		public static float eq_in_out_quad(float t) {
-			t *= 2f;
-			if (t < 1f) return .5f * t * t;
-			t -= 1f;
-			return -.5f * (t * (t - 2f) - 1f);
-		}
-		public static float eq_in_cubic(float t) {
-			return t * t * t;
-		}
-		public static float eq_out_cubic(float t) {
-			t -= 1f;
-			return t * t * t + 1f;
-		}
-		public static float eq_in_out_cubic(float t) {
-			t *= 2f;
-			if (t < 1f) return .5f * t * t * t;
-			t -= 2f;
-			return .5f * (t * t * t + 2f);
-		}
-		public static float eq_in_quart(float t) {
-			return t * t * t * t;
-		}
-		public static float eq_out_quart(float t) {
-			t -= 1f;
-			return -1f * (t * t * t * t - 1f);
-		}
-		public static float eq_in_out_quart(float t) {
-			t *= 2f;
-			if (t < 1f) return .5f * t * t * t * t;
-			t -= 2f;
-			return -.5f * (t * t * t * t - 2f);
-		}
-		public static float eq_in_quint(float t) {
-			return t * t * t * t * t;
-		}
-		public static float eq_out_quint(float t) {
-			t -= 1f;
-			return t * t * t * t * t + 1f;
-		}
-		public static float eq_in_out_quint(float t) {
-			t *= 2f;
-			if (t < 1f) return .5f * t * t * t * t * t;
-			t -= 2f;
-			return .5f * (t * t * t * t * t + 2f);
-		}
-		public static float eq_in_sine(float t) {
-			return -1f * cos(t * PI2) + 1f;
-		}
-		public static float eq_out_sine(float t) {
-			return sin(t * PI2);
-		}
-		public static float eq_in_out_sine(float t) {
-			return (cos(PI * t) - 1f) / -2f;
-		}
-		public static float eq_in_expo(float t) {
-			return t == 0 ? 0 : pow(2, 10f * (t - 1));
-		}
-		public static float eq_out_expo(float t) {
-			return t == 1 ? 1 : -pow(2, -10f * t) + 1;
-		}
-		public static float eq_in_out_expo(float t) {
-			if (t == 0f || t == 1f) return t;
-			t *= 2f;
-			if (t < 1f) return .5f * pow(2, 10f * (t - 1));
-			t -= 1f;
-			return .5f * (-pow(2, -10f * t) + 2f);
-		}
-		public static float eq_in_circ(float t) {
-			return -1f * (sqrt(1f - t * t) - 1f);
-		}
-		public static float eq_out_circ(float t) {
-			t -= 1f;
-			return sqrt(1f - t * t);
-		}
-		public static float eq_in_out_circ(float t) {
-			t *= 2f;
-			if (t < 1f) return -.5f * (sqrt(1f - t * t) - 1f);
-			t -= 2f;
-			return .5f * (sqrt(1f - t * t) + 1f);
-		}
 
 		public static List<Equation> all;
 
@@ -128,6 +128,15 @@ partial class all {
 		public static Equation fromNumber(int number) {
 			foreach (Equation e in all) {
 				if (number == e.number) {
+					return e;
+				}
+			}
+			throw new Exception("boo!");
+		}
+
+		public static Equation fromEquation(Eq eq) {
+			foreach (Equation e in all) {
+				if (eq == e.calc) {
 					return e;
 				}
 			}
--- ztunnel.cs
+++ ztunnel.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Text;
 
 namespace osusb1 {
@@ -18,9 +19,25 @@ partial class all {
 		const int SEGSPACINGMOD = 5;
 		const float ANGINC = PI / CIRCLEAMOUNT;
 
+		const float FADESTART = 300f;
+		const float FADEDIF = 400f;
+		const float FADEEND = FADESTART + FADEDIF;
+
+		const float LIGHTSTARTPOS = 50f;
+		const float LIGHTSPEEDMOD = 2.2f; // higher = faster
+		const int LIGHTFALLOFFTIME = 350;
+
+		List<int> lighttimes = new List<int>();
+		vec3 color = v3(.4f, .1f, .9f);
+
 		public Ztunnel(int start, int stop) {
 			this.start = start;
 			this.stop = stop;
+
+			lighttimes.Add(21000);
+			lighttimes.Add(29750);
+			lighttimes.Add(38350);
+			lighttimes.Add(47050);
 
 			const int ssettings =
 				Sprite.INTERPOLATE_MOVE | Sprite.EASE_FADE | Sprite.EASE_SCALE;
@@ -51,16 +68,23 @@ partial class all {
 					p.z += sin(ang) * RAD;
 					points[idx] = p;
 					dots[idx] = new Odot(Sprite.SPRITE_DOT_6_12, ssettings);
-					dots[idx].addCommandOverride(new ColorCommand(start + 10000, start + 20000, v3(1f), v3(1f, 0f, 0f)));
+					foreach (int lighttime in lighttimes) {
+						int lightstart = lightStartTime(p, lighttime, lighttime);
+						int lightend = lightstart + LIGHTFALLOFFTIME;
+						ColorCommand cc;
+						cc = new ColorCommand(lightstart, lightend, v3(1f), color);
+						cc.easing = Equation.fromEquation(eq_in_quad).number;
+						dots[idx].addCommandOverride(cc);
+					}
 				}
 			}
 		}
 
 		public override void draw(SCENE scene) {
 			ICommand.round_scale_decimals.Push(1);
-			vec3 movement = v3(0f, scene.progress * -600f, 0f);
 			for (int i = 0; i < points.Length; i++) {
-				_points[i] = points[i] + movement;
+				_points[i] = v3(points[i]);
+				_points[i].y = pointYPosAt(points[i], scene.time);
 			}
 
 			float _rot = scene.reltime / 50f;
@@ -69,17 +93,41 @@ partial class all {
 
 			for (int i = 0; i < points.Length; i++) {
 				vec4 q = p.Project(_points[i]);
-				const float FOS = 250f;
-				const float FOE = 650f;
-				float mod = (1f - (clamp(q.w, FOS, FOE) - FOS) / (FOE - FOS));
+				float mod = (1f - (clamp(q.w, FADESTART, FADEEND) - FADESTART) / FADEDIF);
 				float size = 8f * mod;
-				vec4 col = v4(.4f, .1f, .9f, 1f);
+				vec3 color = this.color;
+				if (!rendering) {
+					// it's done using command overrides (see ctor),
+					// so don't do this when exporting.
+					// this is purely for the preview (why am I even doing this)
+					foreach (int lighttime in lighttimes) {
+						int lightstart = lightStartTime(points[i], scene.time, lighttime);
+						int lightend = lightstart + LIGHTFALLOFFTIME;
+						if (lightstart < scene.time && scene.time < lightend) {
+							float x = progress(lightstart, lightend, scene.time);
+							color = lerp(v3(1f), color, x);
+						}
+					}
+				}
+				vec4 col = v4(color, 1f);
 				col.w = mod;
 				col.w *= clamp(scene.reltime, 0f, 400f) / 400f;
 				dots[i].update(scene.time, col, q, size);
 				dots[i].draw(scene.g);
 			}
 			ICommand.round_scale_decimals.Pop();
+		}
+
+		private float pointYPosAt(vec3 point, int time) {
+			float progress = (float) (time - start) / (stop - start);
+			float movement = progress * -600f;
+			return point.y + movement;
+		}
+
+		private int lightStartTime(vec3 point, int time, int lighttime) {
+			float pp = pointYPosAt(point, lighttime);
+			float dif = LIGHTSTARTPOS - pp;
+			return lighttime + (int) (dif / LIGHTSPEEDMOD);
 		}
 
 		public override void fin(Writer w) {
